dbab00dbd8f016f0ddd95a96c3bf979f646d73e0
Andux <nekit991tka2017@gmail.com>
2022-10-17 01:22:40 +0300

    hello word

---
+++ src/test/java/BubleSortTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BubleSortTest {
+
+    @Test
+    void sort() {
+    }
+
+    @Test
+    void copyArrays() {
+    }
+
+    @Test
+    void isSorted() {
+    }
+
+    @Test
+    void sorting() {
+    }
+}
